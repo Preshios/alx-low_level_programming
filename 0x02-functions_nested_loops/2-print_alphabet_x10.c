86c907c528b3324a69404c9077ce3cc1dfd6f707
Precious Ayetigbo <preciousteve12@gmail.com>
2023-07-19 15:51:32 -0700

    Updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	char letter;
 	int n;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (n = 0; n < 10; n++)
 	{
-		for (n = 0; n < 10; n++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 			_putchar('\n');
