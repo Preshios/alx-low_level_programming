4449f48fb74062893ab8c668bf7fdcdc4a50ddd1
Precious Ayetigbo <preciousteve12@gmail.com>
2023-08-03 17:35:21 -0700

    Update on recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns length of a string
+ * @s: The string I want to measure
+ *
+ * Return: The length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+}
